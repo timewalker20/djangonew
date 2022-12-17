b6ca1de483772c1d476b4262e77ca7f7c7757328
Dhiraj <dhirajwasu120@gmail.com>
2022-12-17 22:34:00 +0530

    modified:   cncproject/__pycache__/urls.cpython-310.pyc 	modified:   cncproject/urls.py

--- cncproject/urls.py
+++ cncproject/urls.py
@@ -13,9 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.conf import settings 
-from django.views.static import serve
+
 from django.contrib import admin
 from django.urls import path,include
 from cncapp import views
@@ -24,6 +22,5 @@ urlpatterns = [
     # path('', views.index,name='index'),
     path('admin/', admin.site.urls),
     path('cncapp/',include('cncapp.urls')),
-    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    
 ]
